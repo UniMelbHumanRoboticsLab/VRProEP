efc6cc25a9a181dcbe09146a7111c824f81a52d5
tianshi-yu <54522764+tianshi-yu@users.noreply.github.com>
2023-04-15 13:09:27 +1000

    Add serial comm

---
+++ VRProEP/Assets/2_Scripts/4_GameEngineCore/5_GameMasters/6_Misc/SerialTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialTester : MonoBehaviour
+{
+
+    private SerialDeviceManager serialDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialDevice = new SerialDeviceManager("COM7", 115200);
+        serialDevice.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        serialDevice.Stop();
+    }
+}
---
+++ VRProEP/Assets/2_Scripts/6_Utilities/2_Communications/SerialDeviceManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+using System.Threading;
+
+public class SerialDeviceManager : RunAbleThread
+{
+    private SerialPort port;
+
+    public SerialDeviceManager(string com, int baudrate)
+    {
+        InitPort(com, baudrate);
+        port.Open();
+        
+    }
+
+    //
+    // Overide the run thread
+    //
+    protected override void Run()
+    {
+        while (Running)
+        {
+            try
+            {
+                string data = port.ReadLine();
+                Debug.Log("Serial received: " + data);
+            }
+            catch (System.TimeoutException) { }
+            catch (ThreadAbortException) { return; }
+        }
+        port.Close();
+    }
+
+
+    //
+    // Setup serial port
+    //
+    private void InitPort(string com, int baudrate)
+    {
+        // Connect to the first
+        port = new SerialPort(com);
+
+        // Set the baud rate, parity, data bits, and stop bits
+        port.BaudRate = baudrate;
+        port.Parity = Parity.None;
+        port.DataBits = 8;
+        port.StopBits = StopBits.One;
+    }
+
+}
