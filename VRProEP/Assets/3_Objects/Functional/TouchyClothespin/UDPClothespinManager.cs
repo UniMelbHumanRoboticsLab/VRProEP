baf057ad80192e5411db427a30c609dcebcb8ee0
tianshi-yu <54522764+tianshi-yu@users.noreply.github.com>
2023-04-16 22:19:05 +1000

    Add wireless tactile button for clothespin state reading

--- VRProEP/Assets/3_Objects/Functional/TouchyClothespin/UDPClothespinManager.cs
+++ VRProEP/Assets/3_Objects/Functional/TouchyClothespin/UDPClothespinManager.cs
@@ -5,8 +5,22 @@ using VRProEP.Utilities;
 
 public class UDPClothespinManager : UDPSensorManager
 {
-    public UDPClothespinManager(string ipAddress, int port, string deviceName): base(ipAddress, port, "ClothesPinWiFi") {}
+    public UDPClothespinManager(string ipAddress, int port): base(ipAddress, port, "ClothesPinWiFi") {}
 
+    public enum ClothespinState { Open, Pinch};
+
+    public ClothespinState GetClothespinState()
+    {
+        // Get current sensor data from memory.
+        float[] value = GetCurrentSensorValues();
+        //Debug.Log("Reading length: " + value.Length + ". First value: " + value[0] );
+        ClothespinState state = ClothespinState.Open;
+
+        if (value[0] == 1)
+            state = ClothespinState.Pinch;
+
+        return state;
+    }
 
 
 
---
+++ VRProEP/Assets/3_Objects/Functional/TouchyClothespin/UDPClothespinTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UDPClothespinTester : MonoBehaviour
+{
+    private UDPClothespinManager udpClopin;
+    private string ipAddress = "192.168.137.75";
+    private int port = 2390;
+    // Start is called before the first frame update
+    void Start()
+    {
+        udpClopin = new UDPClothespinManager(ipAddress, port);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UDPClothespinManager.ClothespinState state = udpClopin.GetClothespinState();
+        Debug.Log("Current clothespin state: " + state.ToString());
+    }
+
+    private void OnApplicationQuit()
+    {
+        udpClopin.StopSensorReading();
+    }
+}
